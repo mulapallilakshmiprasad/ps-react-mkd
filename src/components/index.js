c43debf636d6a1a6e9e3e7f3d57463e9433d347a
manojkumardesai <manojkumardbe@gmail.com>
2020-07-16 18:28:51 +0530

    build configuration

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export {default as MDContentExplorer} from './MDContentExplorer';
+export {default as MDContentOpenWith} from './MDContentOpenWith';
+export {default as MDContentPicker} from './MDContentPicker';
+export {default as MDContentPreview} from './MDContentPreview';
+export {default as MDContentSidebar} from './MDContentSidebar';
+export {default as MDContentUploader} from './MDContentUploader';
